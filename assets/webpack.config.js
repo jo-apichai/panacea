d2ce2569dc8716822c467a83eca852eaaa8bed99
Jo Apichai <jo.midnight@gmail.com>
2018-07-24 01:21:06 +0700

    Add fonts
    Add font files and update webpack config to copy file to the correct
    folder when build assets.

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -1,19 +1,25 @@
 const path = require('path')
 const webpack = require('webpack')
 
-const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
 const devMode = process.env.NODE_ENV !== 'production'
 
 
 module.exports = {
   entry: {
-    'app': ['./js/app.js', './css/app.scss']
+    'app': ['./js/app.js', './css/app.scss'],
+    'admin': ['./js/admin.js', './css/admin.scss']
   },
   output: {
     path: path.resolve(__dirname, '../priv/static/'),
     filename: 'js/[name].js'
   },
   plugins: [
+    new CopyWebpackPlugin([
+      { from: './static/**/*', to: '../' },
+    ]),
     new MiniCssExtractPlugin({
       filename: devMode ? 'css/[name].css' : 'css/[name].[hash].css',
       chunkFilename: devMode ? 'css/[id].css' : 'css/[id].[hash].css'
